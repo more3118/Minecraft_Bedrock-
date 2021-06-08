484780bc90391de7d3e8e17e2faa4ba5819adf6c
more3118 <67715407+more3118@users.noreply.github.com>
2021-06-08 09:56:00 -0500

    Bdsgen.py

---
+++ BDS-generator file.py
@@ -0,0 +1,16 @@
+"BDS server.properties maker"
+a=input("What should the name of the server be ?")
+
+b=input("What is the")
+
+
+def name():
+    if x == 0
+        print("")
+
+
+
+with open ("server.properties","w") as f:
+    write
+
+
